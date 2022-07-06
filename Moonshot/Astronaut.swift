a0bf2ad428a1d744b39c643acb59adb8acfbeb64
NT <usedforfunny@gmail.com>
2022-07-06 15:56:00 +0200

    Mission logos are shown in lazyVGrid

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Alexandre Talatinian on 05/07/2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
